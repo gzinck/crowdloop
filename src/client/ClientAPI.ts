d471c1d119d07e7be5ea39e6089dca086f796281
Graeme Zinck <graeme.zinck@gmail.com>
2021-11-14 21:32:40 -0500

    Configure server

--- src/client/ClientAPI.ts
+++ src/client/ClientAPI.ts
@@ -4,6 +4,8 @@ import AudioAPI from './AudioAPI';
 import ClockAPI from './ClockAPI';
 import SessionAPI from './SessionAPI';
 
+const URL = 'ws://localhost:2000';
+
 class ClientAPI {
   public readonly audio: AudioAPI;
   public readonly sessionID: string;
@@ -12,7 +14,7 @@ class ClientAPI {
   public readonly audience: AudienceAPI;
 
   constructor(ctx: AudioContext, sessionID: string) {
-    const socket = io(process.env.REACT_APP_SERVER_URL as string);
+    const socket = io(process.env.REACT_APP_SERVER_URL || URL);
     this.sessionID = sessionID;
     this.audio = new AudioAPI(socket, sessionID);
     this.session = new SessionAPI(socket, sessionID);
